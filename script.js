c7595073550091ea359fcc421cce6b2f94290dd5
SOMK17 <seanankerr@gmail.com>
2024-09-29 16:43:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,30 @@ function openCurtain() {
         document.body.style.overflow = 'auto'; // Allow scrolling after curtains open
     }, 2000); // Delay to match curtain animation duration
 }
+
+
+// Gallery function
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+    
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+
+    slides[slideIndex - 1].style.display = "block";  
+}
